9a3c332013ac841534ebf28c88e3a83d8f2f1796
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2025-09-10 13:08:13 +0200

    add stub docstrings to improve the API ref landing page

--- tunix/sft/dpo/dpo_trainer.py
+++ tunix/sft/dpo/dpo_trainer.py
@@ -89,6 +89,9 @@ class TrainExample:
 
 @dataclasses.dataclass(slots=True, kw_only=True)
 class DPOTrainingConfig(peft_trainer.TrainingConfig):
+  """DPO Training Config.
+  """
+
   beta: float = 0.1  # 𝛽 for KL penalty https://arxiv.org/pdf/2305.18290
   label_smoothing: float = 0.0
 
